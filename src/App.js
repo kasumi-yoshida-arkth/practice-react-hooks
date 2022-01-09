8d4fca7c393c6663153af9ce667a2299846e124b
kasumi_yoshida <k.yoshida@arkth.co.jp>
2022-01-09 23:36:48 +0900

    use multiple-states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 
-const App = () => {
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
-    <div>
-      This is a template for react app.
-    </div>
+    <>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+    </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
