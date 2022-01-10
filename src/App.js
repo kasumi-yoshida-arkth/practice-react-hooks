cac590c726876557a1c9ea1edc5f0736d9b8bc22
kasumi_yoshida <k.yoshida@arkth.co.jp>
2022-01-10 23:14:00 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 
-const App = () => {
+const App = props => {
+  const [state, setState] = useState(props)
+  const { name, price } = state
+
   return (
-    <div>
-      This is a template for react app.
-    </div>
+    <>
+      <p>現在の{name}は、{state.price}円です。</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
+    </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
