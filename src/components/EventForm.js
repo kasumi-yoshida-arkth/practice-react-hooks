3be1c18c3f419c4b7ffc8eb5306909846041dbac
kasumi_yoshida <k.yoshida@arkth.co.jp>
2022-02-23 16:38:35 +0900

    solve prop drilling issue

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,52 +1,54 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 
 import {
-  CREATE_EVENT,
-  DELETE_ALL_EVENTS
+    CREATE_EVENT,
+    DELETE_ALL_EVENTS
 } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const EventForm = ({ state, dispatch }) => {
+const EventForm = () => {
+    const { state, dispatch } = useContext(AppContext)
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
   
     const addEvent = e => {
-      e.preventDefault()
-      
-      dispatch({
-        type: CREATE_EVENT,
-        title,
-        body
-      })
-  
-      setTitle('')
-      setBody('')
+        e.preventDefault()
+
+        dispatch({
+            type: CREATE_EVENT,
+            title,
+            body
+        })
+
+        setTitle('')
+        setBody('')
     }
   
     const deleteAllEvents = e => {
-      e.preventDefault()
-      const result = window.confirm('全てのイベントを本当に削除してもいいですか？')
-      if (result) dispatch({ type: DELETE_ALL_EVENTS })
+        e.preventDefault()
+        const result = window.confirm('全てのイベントを本当に削除してもいいですか？')
+        if (result) dispatch({ type: DELETE_ALL_EVENTS })
     }
-  
+
     const unCreatable = title === '' || body === '';
-  
+
     return (
         <>
-          <h4>Form</h4>
-          <form>
-            <div className="form-group col-auto">
-                <label htmlFor="formEventTitle">タイトル</label>
-                <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
-            </div>
-    
-            <div className="form-group col-auto">
-                <label htmlFor="formEventBody">ボディ</label>
-                <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
-            </div>
-    
-            <button className="btn btn-primary col-auto mr-2" onClick={addEvent} disabled={unCreatable}>Submit</button>
-            <button className="btn btn-danger col-auto" onClick={deleteAllEvents} disabled={state.length === 0}>Delete all events</button>
-          </form>
+            <h4>Form</h4>
+            <form>
+                <div className="form-group col-auto">
+                    <label htmlFor="formEventTitle">タイトル</label>
+                    <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
+                </div>
+        
+                <div className="form-group col-auto">
+                    <label htmlFor="formEventBody">ボディ</label>
+                    <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
+                </div>
+
+                <button className="btn btn-primary col-auto mr-2" onClick={addEvent} disabled={unCreatable}>Submit</button>
+                <button className="btn btn-danger col-auto" onClick={deleteAllEvents} disabled={state.length === 0}>Delete all events</button>
+            </form>
         </>
     )
 }
